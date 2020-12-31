605f06b7f25f710201eaaadd7d45f28429196cf2
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 16:57:49 +0900

    seeds.rbを修正: Therapistのインスタンスを5名作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,15 @@ Therapist.create!(unique_id: "10101010",
 )
 admin = Therapist.first
 admin.add_role :admin
+
+5.times do |n|
+  unique_id = "#{n}" * 8
+  first_name = Faker::Name.last_name
+  last_name = Faker::Name.first_name
+  password = "password"
+  Therapist.create!(unique_id: unique_id,
+                    first_name: first_name,
+                    last_name: last_name,
+                    password: password,
+                    password_confirmation: password)
+end
