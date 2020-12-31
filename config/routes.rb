3a881efd940a81e2dd117c464f2c17a6c5885c65
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 11:34:45 +0900

    routes.rbを修正: therapistのサインアップ,ログイン,ログアウトのパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
     sessions: "therapists/sessions",
     registrations: "therapists/registrations"
   }
+
+  devise_scope :therapist do
+    root to: "therapists/sessions#new"
+    get "/signup", to: "therapists/registrations#new"
+    post "/login", to: "therapists/sessions#create"
+    delete "/logout", to: "therapists/sessions#destroy"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
