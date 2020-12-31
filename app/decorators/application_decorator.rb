1c5da46d7ebfe798dde2f852755ae6e3de054dea
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 16:00:19 +0900

    Therapist decorator specを作成 & Application_decoratorを修正: 作成日を見やすく表示するメソッドを定義

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,8 +1,7 @@
 class ApplicationDecorator < Draper::Decorator
-  # Define methods for all decorated objects.
-  # Helpers are accessed through `helpers` (aka `h`). For example:
-  #
-  #   def percent_amount
-  #     h.number_to_percentage object.amount, precision: 2
-  #   end
+  delegate_all
+
+  def date_of_creation
+    created_at.strftime('%Y/%m/%d')
+  end
 end
