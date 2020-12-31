c1f876de32aecc3b82fdaf87f70583972aa0c5b7
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 18:58:56 +0900

    patient modelを修正: sexカラムを列挙型に定義

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,9 @@
 class Patient < ApplicationRecord
   include Scope
-  
+
   belongs_to :therapist
   validates :first_name, presence: true, length: { maximum: 10 }
   validates :last_name, presence: true, length: { maximum: 10 }
+
+  enum sex: { undefined: nil, man: 1, woman: 2 }
 end
