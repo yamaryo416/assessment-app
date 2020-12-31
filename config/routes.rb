61312dd237cc1acd21d4b084babccd51cb904b61
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 11:32:16 +0900

    routes.rbを修正: deviseのsessionsとregistrationsコントローラーをカスタムしたものを使用することを記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :therapists
+  devise_for :therapists, controllers: {
+    sessions: "therapists/sessions",
+    registrations: "therapists/registrations"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
