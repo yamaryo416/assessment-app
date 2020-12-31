a9fd1c367e5bc2f99b76ff323e406f079c6f4099
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 14:29:55 +0900

    config/application.rbを修正: デフォルトの言語を日本語に設定 & locals/jaファイルを読み込むように背一定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module HyoukaApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +20,6 @@ module HyoukaApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
