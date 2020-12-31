efcb84c94167efae11c67e4e9cf8e099243c95d0
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 15:54:35 +0900

    Application helper specを作成

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe ApplicationHelper, type: :helper do
+  
+  it "provide flash message" do
+    expect(flash_message("ログインしました。", :success)).to eq(
+      "<div class=\"alert alert-success\">"\
+      "<button class=\"close\" data-dismiss=\"alert\">x</button>ログインしました。</div>"
+    )
+  end
+end
