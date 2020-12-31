2278a311f11c6c7d7d35a381b70428401fe24259
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 18:50:17 +0900

    migrate/create_patientsを修正: first_name,last_name,age,sex,height,weightを追加

--- db/migrate/20201231094555_create_patients.rb
+++ db/migrate/20201231094555_create_patients.rb
@@ -1,6 +1,12 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
     create_table :patients do |t|
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.integer :age, default: nil
+      t.integer :sex
+      t.float :height
+      t.float :weight
       t.references :therapist, null: false, foreign_key: true
 
       t.timestamps
