d378004090bfe406d7349659cd95e47926202c8f
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 11:44:12 +0900

    therapists/registrations_controller.rbを修正: サインアップ時にunique_id,first_name,last_nameパラムを許可

--- app/controllers/therapists/registrations_controller.rb
+++ app/controllers/therapists/registrations_controller.rb
@@ -38,12 +38,12 @@ class Therapists::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:unique_id, :first_name, :last_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
