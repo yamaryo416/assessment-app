315cae5489b58b824fcf77fbc4c2cdad7317719d
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 18:55:36 +0900

    patient modelを修正:first_name,last_nameのvalidationを追加 & concerns/scope.rbをinclude

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,7 @@
 class Patient < ApplicationRecord
+  include Scope
+  
   belongs_to :therapist
+  validates :first_name, presence: true, length: { maximum: 10 }
+  validates :last_name, presence: true, length: { maximum: 10 }
 end
