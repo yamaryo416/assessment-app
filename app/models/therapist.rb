697d6680907a5b4a329e7b4e0ef4a3bc93889bb5
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2020-12-31 11:22:37 +0900

    modeles/therapist.rbを修正: deviseのrememberableを削除 & therapist登録,認証,変更時にemailカラムを不要とする

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -2,5 +2,17 @@ class Therapist < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
+
+  def will_save_change_to_email?
+    false
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
