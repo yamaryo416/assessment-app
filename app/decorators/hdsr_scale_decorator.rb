8feced5439b0a764426f7e5b6022d709e4f79257
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2021-01-01 23:24:45 +0900

    hdsr scale decoratorを作成

--- app/decorators/hdsr_scale_decorator.rb
+++ app/decorators/hdsr_scale_decorator.rb
@@ -1,13 +1,17 @@
-class HdsrScaleDecorator < ApplicationDecorator
+# frozen_string_literal: true
+
+class HdsrScaleDecorator < ScaleDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def total_score
+    total_score = 0
+    attributes.each do |attr_name, value|
+      if EXCLUDE_COLUMNS.include?(attr_name) || value.nil?
+        next
+      else
+        total_score += value
+      end
+    end
+    total_score
+  end
 end
