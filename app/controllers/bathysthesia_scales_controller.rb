12a1660dc1f0f5ebc097ff378462fad695f741ef
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2021-01-01 20:10:27 +0900

    bathysthesia scales controllerを修正: index,show,new,create,edit,update,destroyアクションを定義

--- app/controllers/bathysthesia_scales_controller.rb
+++ app/controllers/bathysthesia_scales_controller.rb
@@ -1,2 +1,54 @@
-class BathysthesiaScalesController < ApplicationController
+class BathyesthesiaScalesController < ApplicationController
+  before_action :set_bathyesthesia_scale, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bathyesthesia_scales = BathyesthesiaScaleDecorator.
+      decorate_collection(@patient.bathyesthesia_scales.recent)
+  end
+
+  def show
+  end
+
+  def new
+    @bathyesthesia_scale = BathyesthesiaScale.new.decorate
+  end
+
+  def create
+    @bathyesthesia_scale = @patient.bathyesthesia_scales.build(bathyesthesia_scale_params)
+    @bathyesthesia_scale.save
+    flash[:success] = "深部感覚検査を登録しました。"
+    redirect_to patient_bathyesthesia_scales_path(@patient)
+  end
+
+  def edit
+  end
+
+  def update
+    @bathyesthesia_scale.update(bathyesthesia_scale_params)
+    flash[:success] = "深部感覚検査を編集しました。"
+    redirect_to patient_bathyesthesia_scales_path(@patient)
+  end
+
+  def destroy
+    @bathyesthesia_scale.destroy
+    flash[:success] = "深部感覚検査を削除しました。"
+    redirect_to patient_bathyesthesia_scales_path(@patient)
+  end
+
+  private
+
+  def bathyesthesia_scale_params
+    params.require(:bathyesthesia_scale).permit(:right_upper_limb,
+                                                :left_upper_limb,
+                                                :right_lower_limb,
+                                                :left_lower_limb,
+                                                :right_finger,
+                                                :left_finger,
+                                                :right_toe,
+                                                :left_toe)
+  end
+
+  def set_bathyesthesia_scale
+    @bathyesthesia_scale = @patient.bathyesthesia_scales.find(params[:id]).decorate
+  end
 end
