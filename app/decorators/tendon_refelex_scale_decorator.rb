6bdf96dbb0fb0750f706aa6d1323f0e60a69f593
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2021-01-01 18:46:29 +0900

    tendon reflex scale decoratorを作成

--- app/decorators/tendon_refelex_scale_decorator.rb
+++ app/decorators/tendon_refelex_scale_decorator.rb
@@ -1,13 +1,29 @@
-class TendonRefelexScaleDecorator < ApplicationDecorator
+# frozen_string_literal: true
+
+class TendonReflexScaleDecorator < ScaleDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def hyperreflexia_part
+    hyperreflexia_part = []
+    scale_score.each do |scale|
+      next if scale[1].nil?
+      value = send("#{scale[0]}_before_type_cast")
+      if value >= 3
+        hyperreflexia_part << scale
+      end
+    end
+    hyperreflexia_part
+  end
 
+  def hyporeflexia_part
+    hyporeflexia_part = []
+    scale_score.each do |scale|
+      next if scale[1].nil?
+      value = send("#{scale[0]}_before_type_cast")
+      if value <= 1
+        hyporeflexia_part << scale
+      end
+    end
+    hyporeflexia_part
+  end
 end
