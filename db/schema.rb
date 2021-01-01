e69c10b95e9cb66b47d24a9c74b5abe7879beea0
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2021-01-01 17:23:47 +0900

    migrate/create_nrsを修正: rating,status,supplementカラムを追加 & nrs scale modelを修正: statusを列挙型に設定とratingのvalidationを追加

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_064216) do
+ActiveRecord::Schema.define(version: 2021_01_01_081810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,16 @@ ActiveRecord::Schema.define(version: 2021_01_01_064216) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["patient_id"], name: "index_mmt_scales_on_patient_id"
+  end
+
+  create_table "nrs_scales", force: :cascade do |t|
+    t.integer "rating"
+    t.integer "status"
+    t.string "supplement"
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_nrs_scales_on_patient_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -197,6 +207,7 @@ ActiveRecord::Schema.define(version: 2021_01_01_064216) do
   end
 
   add_foreign_key "mmt_scales", "patients"
+  add_foreign_key "nrs_scales", "patients"
   add_foreign_key "patients", "therapists"
   add_foreign_key "rom_scales", "patients"
 end
