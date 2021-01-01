f5bbd2a0f1ad79b85bb88693af047d42d31bb6e6
Ryo Yamaguchi <61639293+yamaryo416@users.noreply.github.com>
2021-01-01 21:28:54 +0900

    sias scale decoratorを作成

--- app/decorators/sias_scale_decorator.rb
+++ app/decorators/sias_scale_decorator.rb
@@ -1,13 +1,22 @@
-class SiasScaleDecorator < ApplicationDecorator
+# frozen_string_literal: true
+
+class SiasScaleDecorator < ScaleDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def total_score
+    total_score = 0
+    attributes.each do |attr_name, value|
+      if EXCLUDE_COLUMNS.include?(attr_name) || value.nil?
+        next
+      else
+        score = send("#{attr_name}_before_type_cast")
+        if !score.integer?
+          total_score += score.floor
+        else
+          total_score += score
+        end
+      end
+    end
+    total_score
+  end
 end
